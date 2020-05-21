56a465fbea931ea9a09cc5b623c49ab388b1d650
Juansebastian987 <juanse.bastian2012@hotmail.com>
2020-05-21 15:32:33 -0500

    Commit

--- src/test/java/co/edu/icesi/fi/tics/tssc/delegate/JUnitTopicDelegate.java
+++ src/test/java/co/edu/icesi/fi/tics/tssc/delegate/JUnitTopicDelegate.java
@@ -2,10 +2,27 @@ package co.edu.icesi.fi.tics.tssc.delegate;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+import org.mockito.junit.MockitoJUnitRunner;
 
+import com.example.demo.delegate.TopicDelegateImp;
+
+@RunWith(MockitoJUnitRunner.class)
 class JUnitTopicDelegate {
+	
+	@Mock
+	private TopicDelegateImp topicDelegate;
 
+	@BeforeEach
+	void setup() {
+		MockitoAnnotations.initMocks(this);
+	}
+	
+	
 	@Test
 	void testGET() {
 		fail("Not yet implemented");
